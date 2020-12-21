9018d5ab2167466d15557a28a845ddd11dfbfdad
Vinicius barros da silva <barros.vinicius0103@gmail.com>
2020-12-21 12:23:44 -0300

    Ajustes no ambiente de produção

--- knexfile.js
+++ knexfile.js
@@ -22,19 +22,12 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DB_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './src/data/migrations',
+    },
+    seeds: { directory: './src/data/seeds' },
   }
 
 };
