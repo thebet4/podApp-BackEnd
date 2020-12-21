b1081f6c4ebda0ce2d736efbb6e30849f38ee7c9
Vinicius barros da silva <barros.vinicius0103@gmail.com>
2020-12-21 11:29:35 -0300

    Ajustes para deploy

--- knexfile.js
+++ knexfile.js
@@ -13,19 +13,12 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DB_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './src/data/migrations',
+    },
+    seeds: { directory: './src/data/seeds' },
   },
 
   production: {
